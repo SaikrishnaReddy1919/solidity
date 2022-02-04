d5f0909a077795902652e0850f28fc6feacfb8ad
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2022-02-04 22:52:36 +0530

    overriding - virtual & override

--- contracts/Inheritance.sol
+++ contracts/Inheritance.sol
@@ -2,7 +2,9 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract A {
-    function getMyName() public pure returns (string memory) {
+    // function which can allowed to be overrided by other contract function must be defined with 
+    // keyword 'virtual'
+    function getMyName() virtual public pure returns (string memory) {
         return 'Am Contract A';
     }
 }
@@ -12,3 +14,15 @@ contract B is A {
 
 }
 // deploy the contract B in remix and see that deployed code will have getMyName functione.
+
+
+
+// <----- Overriding ----->
+contract C is A {
+    // function which overrides the other contract functions must be defined with keyword 'override'
+    // and when overriding, function signature must be same otherwise new function will created along with
+    // the function from inherited contract.
+    function getMyName() override public pure returns (string memory) {
+        return 'Am Contract A';
+    }
+}
