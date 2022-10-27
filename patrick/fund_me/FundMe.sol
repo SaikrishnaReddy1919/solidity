0114a95dd32a53fdecd7b7ff1a0a59873ae2ee60
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2022-10-27 19:14:21 +0530

    withdraw funds, modifier - onlyowner

--- patrick/fund_me/FundMe.sol
+++ patrick/fund_me/FundMe.sol
@@ -14,7 +14,11 @@ contract FundMe {
     uint256 public minimumUSD = 50 * 1e18; // usd with 18 decimals
     address[] public funders;
     mapping(address => uint256) public addressToAmountFounded;
+    address public owner;
 
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable {
         //set min fund amount
@@ -24,7 +28,32 @@ contract FundMe {
         addressToAmountFounded[msg.sender] = msg.value; // keeps track of how much amount sent for each address.
     }
 
-    function withdraw() public{
+    function withdraw() public onlyOwner {
+         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+             address funder = funders[funderIndex];
+             addressToAmountFounded[funder] = 0;
+         }
+         //reset array 
+         funders = new address[](0); //blank new array
+         //withdraw 
+         //three diff ways : transfer(throws error), send(return bool), call(return bool)
 
+         // transfer : transfer automatically reverts if transfer failes and it uses 2300gas. IF used more txn fails
+         //msg.sender = address
+         //payable(msg.sender) = payable address
+        //  payable(msg.sender).transfer(address(this).balance);
+
+         //send : require is needed to check whether send is success or not. Send returns bool and it also uses 2300gas
+        //  bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        //  require(sendSuccess, "Send failed.");
+
+         //call :
+         (bool callSuccess,) = payable(msg.sender).call{value : address(this).balance}("");
+         require(callSuccess, "Call failed.");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner is allowed to call this.");
+        _;
     }
 }
