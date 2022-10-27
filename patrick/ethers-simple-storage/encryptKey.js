2047175d16b60ca00ecf553c1fd43b9249a8c05c
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2022-10-28 01:31:30 +0530

    formatting stuff

--- patrick/ethers-simple-storage/encryptKey.js
+++ patrick/ethers-simple-storage/encryptKey.js
@@ -2,12 +2,14 @@ const ethers = require('ethers')
 const fs = require('fs-extra')
 require('dotenv').config()
 
-
 async function main() {
-    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY)
-    const encryptedJSONKey = await wallet.encrypt(process.env.PRIVATE_KEY_PASSWORD, process.env.PRIVATE_KEY)
-    console.log(encryptedJSONKey)
-    fs.writeFileSync("./.encryptedKey.json", encryptedJSONKey)
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY)
+  const encryptedJSONKey = await wallet.encrypt(
+    process.env.PRIVATE_KEY_PASSWORD,
+    process.env.PRIVATE_KEY,
+  )
+  console.log(encryptedJSONKey)
+  fs.writeFileSync('./.encryptedKey.json', encryptedJSONKey)
 }
 
 main()
