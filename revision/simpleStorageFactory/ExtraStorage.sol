b33033438d213fcff60524c26504b606e37a22a6
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2022-10-27 12:18:42 +0530

    inheritance ex

---
+++ revision/simpleStorageFactory/ExtraStorage.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./SimpleStorage.sol";
+
+//inheritance and override example.
+
+contract ExtraStorage is SimpleStorage {
+    // lets do someting diff here -> +10
+    //override
+    //virtual override
+
+    function store(uint256 _favoriteNumber) public override{
+        favoriteNumber = _favoriteNumber + 10;
+    }
+}
