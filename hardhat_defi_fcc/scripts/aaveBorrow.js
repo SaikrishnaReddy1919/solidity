575397293f30545bbc7ccaef3f6e0149764324dd
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2022-11-13 08:51:32 +0530

    info about how much can borrow, hom much in collateral : lendingPool.getUserAccountData()

--- hardhat_defi_fcc/scripts/aaveBorrow.js
+++ hardhat_defi_fcc/scripts/aaveBorrow.js
@@ -20,6 +20,24 @@ async function main() {
     console.log("Depositing...")
     await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
     console.log("Deposited...")
+
+    //borrow
+    //we must know -> how much we have borrwed, how much in collateral, how much we can borrow
+    let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(lendingPool, deployer)
+}
+
+async function getBorrowUserData(lendingPool, account) {
+    const {
+        totalCollateralETH,
+        totalDebtETH,
+        availableBorrowsETH,
+    } = await lendingPool.getUserAccountData(account)
+
+    console.log(`You have ${totalCollateralETH} worth of ETH deposited.`)
+    console.log(`You have ${totalDebtETH} worth of ETH borrowed.`)
+    console.log(`You can borrow ${availableBorrowsETH} worth of ETH.`)
+
+    return { availableBorrowsETH, totalDebtETH }
 }
 
 async function approveERC20(contractAdd, spenderAdd, amountToSpend, account) {
