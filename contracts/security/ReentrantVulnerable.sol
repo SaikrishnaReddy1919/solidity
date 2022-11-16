e97fb705938f5da4c5065577c96d356e7f2f47eb
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2022-11-16 11:40:47 +0530

    openzeppelin reentrancy guard added

--- contracts/security/ReentrantVulnerable.sol
+++ contracts/security/ReentrantVulnerable.sol
@@ -1,6 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+// Based on https://solidity-by-example.org/hacks/re-entrancy
+
+/*
+ReentrantVulnerable is a contract where you can deposit and withdraw ETH.
+This contract is vulnerable to re-entrancy attack.
+Let's see why.
+
+1. Deploy ReentrantVulnerable
+2. Deposit 1 Ether each from Account 1 (Alice) and Account 2 (Bob) into ReentrantVulnerable
+3. Deploy Attack with address of ReentrantVulnerable
+4. Call Attack.attack sending 1 ether (using Account 3 (Eve)).
+   You will get 3 Ethers back (2 Ether stolen from Alice and Bob,
+   plus 1 Ether sent from this contract).
+
+What happened?
+Attack was able to call ReentrantVulnerable.withdraw multiple times before
+ReentrantVulnerable.withdraw finished executing.
+
+Here is how the functions were called
+- Attack.attack
+- ReentrantVulnerable.deposit
+- ReentrantVulnerable.withdraw
+- Attack fallback (receives 1 Ether)
+- ReentrantVulnerable.withdraw
+- Attack.fallback (receives 1 Ether)
+- ReentrantVulnerable.withdraw
+- Attack fallback (receives 1 Ether)
+*/
+
 contract ReentrantVulnerable {
     mapping(address => uint256) public balances;
 
