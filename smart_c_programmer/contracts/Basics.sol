d182c5e7a4e6d613a28d1e4825473030977ace84
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2022-11-19 15:57:17 +0530

    adv solidity

---
+++ smart_c_programmer/contracts/Basics.sol
@@ -0,0 +1,83 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Basics {
+    bool public b = true;
+    uint public u = 123; //uint = unit256 0 to 2**256 - 1
+                         //uint8 = 0 to 2**8 - 1
+                         //uint16 = 0 to 2**16 - 1
+    int public i = -123  // int = int256 -2**256 to 2**256 - 1
+                         // int = int128 -2**128 to 2**128 - 1
+    int public minInt = type(int).min;
+    int public maxInt = type(int).max;
+
+    address public addr = 0x2821adDd7D2feBd4b3D98012356A922DedB5752A;
+    bytes32 public b32 = 0x2821adDd7D2feBd4b3D98012356A922DedB5752AeBd4b3D98012356A922DedB57;
+
+    //default values
+    bool public boo; //false
+    uint public de; // 0
+    int public in; //0
+    address public add //0x000...00(40 zeros)
+    bytes32 public byt; //0x000...00(64 zeros)
+
+    //Error
+    // revert, assert, require
+    // when error is throw, gas(unused) will be refunded and any changes to state
+    //variables will be reverted/undone.
+    // custom errors - save gas
+
+    function test_require(uint _i) public pure{
+        require(_i < 10, "i must be greated than 10.")
+        //code
+    }
+
+    //revert and require both does the same but
+    //revert is better option when having nested if conditions
+    function test_revert(uint _i) public pure{
+        if(_i > 10) {
+            revert("i > 10");
+        }
+        if(_i > 1) {
+            //code
+            if(_i > 2) {
+                //code
+                if(_i > 10) {
+                    revert("i > 10");
+                }
+            }
+        }
+        //code
+    }
+
+    /**
+     * assert : use assert when the condition must always evaluates to true.
+     * Ex: say you have num state variable and you want it always to be equal
+     *      - to 10. If some function changes its value then assert will be failed and it
+     *      - means there is a some bug in the code.
+     */
+
+    uint public num = 10;
+    uint public count = 10;
+
+    function test_assert() public view {
+        assert(num == 10); // this must be true
+    }
+
+    // modifers
+    modifier whenNotPaused {
+        require(!paused, "Paused.");
+        _;
+    }
+
+    modifier sandwich(uint _x) {
+        count += 10;
+        _;
+        // below code runs after calling function excution completion
+        count *= 2;
+    }
+
+    function check_pauesd_and_x(uint _x) public whenNotPaused sandwich(_x){
+        count += 5;
+    }
+}                        
---
+++ smart_c_programmer/contracts/SomeAdvanced.sol
@@ -0,0 +1,95 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+//return multiple outputs
+// Names outputes
+//Destructuring assignment
+contract SomeAdvanced {
+
+    function output() public pure returns (address, int) {
+        return (address(this), 10)
+    }
+
+    function named_output() public pure returns (address me, int age) {
+        return (address(this), 10)
+    }
+
+    function assigned() public pure returns (address me, int age) {
+        me = address(this);
+        age = 10;
+    }
+
+    function destructure() public pure {
+        (address myAdd, uint256 age) = assigned();
+    }
+
+    //arrays
+    //dynamic or fixed 
+
+    uint[] public nums = [1, 2, 3]; //dynamic
+    uint[5] public fixed_nums = [1, 2, 4]; //fixed - even if the values or not filled, you cant push if after
+
+    function examples() public {
+        nums.push(4); //[1, 2, 3, 4]
+        uint x = nums[1];
+        nums[2] = 22; // [1, 2, 22, 4]
+
+        delete nums[1]; // [1, 0, 22, 4] -> ***** delete replaces the number with 0. Length will be same . To remove completly see below.
+        num.pop(); //[1, 0, 22]
+        uint len = nums.length;
+
+        //create array in memory
+        // must be of fixed size.
+        // you cant push or pop.
+        // can only be updated
+        uint[] memory a = new uint[](5);
+    }
+
+    function returnArray() external view returns (uint[] memory) {
+        return nums;
+    }
+
+
+    // remove an element from array
+    // shift the elements to the left starting from the index then pop the last one.
+    //Ex : [1, 2, 3, 4] -> remove at index 2 -> [1, 2, 4, 4] -> then pop -> [1, 2, 4]
+    //This is very gas expensive
+    uint[] public arr;
+    function remove(uint _index) public  {
+        require(_index < arr.length, "Index out of bounds");
+
+        for(uint i = _index; i < arr.length -1 ; i++){
+            arr[i] = arr[i+1];
+        }
+        arr.pop();
+    }
+
+    function test() public {
+        arr = [1, 2, 3, 4, 5];
+        remove(2);
+
+        assert(arr[0] == 1);
+        assert(arr[1] == 2);
+        assert(arr[2] == 4);
+        assert(arr[3] == 5);
+
+        assert(arr.length == 4);
+
+        arr = [1];
+        remove(0);
+        assert(arr.length == 0);
+    }
+
+    //gas defiecient way.
+    //replace the element that you want to remove with the last element and pop the lost one
+    //no shifting so less gas expensive. But the last element will be copied to removed ele
+    uint[] public demo_arr = [1, 2, 3, 4, 5];
+    function arrayReplaceLast(uint _index) public {
+        //remove ele at index 2
+        demo_arr[_index] = demo_arr[demo_arr.length - 1];
+        demo_arr.pop();
+    }
+    function returnarr() public view returns(uint[] memory) {
+        return demo_arr;
+    }
+}
