5ab2a5ade5bfb0fd316811508a8c1ba152b7a6ed
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2022-12-07 00:09:53 +0530

    assembly in solidity

---
+++ smart_c_programmer/contracts/assembly.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.7;
+
+contract Assembly {
+    uint a;
+    uint b;
+    uint c;
+
+    function foo() external {
+        c = a + b;
+
+        // syntax assembly
+
+        assembly {
+            // to do add in assembly
+            let d := add(1, 2)
+
+            //------- read and store data in assembly -------
+            //read -> load address where f is stored
+            let f := mload(0x40)
+            // to store in temporary storage - memory
+            mstore(f, 2)
+            //to store in permanent storage. i.e, on chain
+            sstore(f, 2)
+        }
+    }
+
+    //detect if address is smart contract or not
+    //TODO: snippet
+    function isSmartContract(address _address) external view returns (bool) {
+        uint size;
+        assembly {
+            size := extcodesize(_address)
+        }
+
+        return size > 0 ? true : false;
+    }
+}
